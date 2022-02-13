3b9752ab4c4ee87decfa891b89ba8c15cc1721c8
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-02-13 23:15:49 +0300

    git init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+ <div class='flex flex-column align-start align-self-start justify-start'>
+   <h1>Welcome to your dashboard</h1>
+   <p>You are now logged in</p>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
