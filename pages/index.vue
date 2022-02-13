b1559aca7acdff612b3fc04fcd3693b4c6040130
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-02-14 00:45:53 +0300

    add sign out

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
  <div class='flex flex-column align-start align-self-start justify-start'>
    <h1>Welcome to your dashboard</h1>
-   <p>You are now logged in</p>
+   <p>You are now logged in - {{ $nuxt.$fire.auth.currentUser.email}}</p>
+   <v-btn @click='$nuxt.$fire.auth.signOut()'>Log out</v-btn>
  </div>
 </template>
 
